0a7cd1e8ce14d0729dce0331ff33c7940c99585a
Chris Ryan <chris.ryan@csiro.au>
2025-02-04 14:58:36 +1100

    Rename bat and prefs to 310

--- IDLDE 91 Python310 GeoPIXE open source.bat
+++ IDLDE 91 Python310 GeoPIXE open source.bat
@@ -9,5 +9,5 @@ call c:\Anaconda3\condabin\conda.bat activate base
 PATH=C:\Program Files\NV5\IDL91\bin\bin.x86_64;C:\Anaconda3;C:\Anaconda3\Scripts;%PATH%
 SET PYTHONPATH=C:\Program Files\NV5\IDL91\bin\bin.x86_64;C:\Program Files\NV5\IDL91\lib\bridges;C:\Anaconda3;C:\software\python\SVN-Maia-Mapper\src\main\python;C:\software\python\SVN-Maia-Mapper\src\test\python;%PYTHONPATH%
 
-"C:\Program Files\NV5\IDL91\bin\bin.x86_64\idlde.exe" -data "C:\Software\IDL\GeoPIXE-open-source\Workspace" -pref="C:\Software\IDL\GeoPIXE-open-source\Workspace\idl91-py311.pref"
+"C:\Program Files\NV5\IDL91\bin\bin.x86_64\idlde.exe" -data "C:\Software\IDL\GeoPIXE-open-source\Workspace" -pref="C:\Software\IDL\GeoPIXE-open-source\Workspace\idl91-py310.pref"
 
