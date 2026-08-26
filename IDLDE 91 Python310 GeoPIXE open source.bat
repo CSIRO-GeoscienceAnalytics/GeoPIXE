8cd38b9d25d1851fdb7b7219835eb42265a256e1
Chris Ryan <chris.ryan.again49@gmail.com>
2026-08-26 17:21:25 +1000

    Update script and prefs.

--- IDLDE 91 Python310 GeoPIXE open source.bat
+++ IDLDE 91 Python310 GeoPIXE open source.bat
@@ -7,16 +7,21 @@ rem "Use IDL 9.1 and python 3.10"
 
 path
 
-call C:\Users\rya113\AppData\Local\miniforge3\condabin\conda.bat deactivate
+rem call C:\Users\chris\miniforge3\condabin\conda.bat deactivate
 
 rem For some extensions, assumes there is a python enviromnment "py310" for python 3.10
 
-call C:\Users\rya113\AppData\Local\miniforge3\condabin\conda.bat activate py310
-rem pause
+call C:\Users\chris\miniforge3\condabin\conda.bat activate py310
 
-PATH=C:\Program Files\nv5\IDL91\bin\bin.x86_64;%PATH%
+rem
+rem "Remember to VPN to UMelb for license before continuing ..."
+pause
 
-SET PYTHONPATH=C:\Program Files\nv5\IDL91\bin\bin.x86_64;C:\Program Files\nv5\IDL91\lib\bridges;C:\software\python\SVN-Maia-Mapper\src\main\python;C:\software\python\SVN-Maia-Mapper\src\test\python;%PYTHONPATH%
+PATH=C:\Program Files\NV5\IDL91\bin\bin.x86_64;%PATH%
 
-"C:\Program Files\nv5\IDL91\bin\bin.x86_64\idlde.exe" -data "C:\Software\IDL\GeoPIXE-open-source\Workspace" -pref="C:\Software\IDL\GeoPIXE-open-source\Workspace\idl91-py310.pref"
+SET PYTHONPATH=C:\Program Files\NV5\IDL91\bin\bin.x86_64;C:\Program Files\NV5\IDL91\lib\bridges;Z:\software\python\SVN-Maia-Mapper\src\main\python;Z:\software\python\SVN-Maia-Mapper\src\test\python;%PYTHONPATH%
+
+"C:\Program Files\NV5\IDL91\bin\bin.x86_64\idlde.exe" -data "Z:\Software\IDL\GeoPIXE\Workspace" -pref="Z:\Software\IDL\GeoPIXE\Workspace\idl91-py310.pref"
+
+pause
 
